7c5af75f8a83b8dc8854a0a45b0643cc196173dc
cadburry <mathis.keyser@it-syn.de>
2024-03-03 16:52:31 +0100

    import_users_simple_and_csv

--- new-users/new_adusers.ps1
+++ new-users/new_adusers.ps1
@@ -1,18 +1,24 @@
 Import-Module ActiveDirectory
 $pfad = "OU=Benutzer,OU=Hogwarts,DC=hogwarts,DC=local"
-Import-Csv ".\new-users\ImportADUsers.csv" -Delimiter ',' |
+$password = "Synergy2024!"
 
-
+Import-Csv "C:\Users\Administrator\Documents\powershell\new-users\ImportADUsers.csv" -Delimiter ',' |
 ForEach-Object { 
-New-ADUser `
--Name $_.DisplayName `
+
+New-ADUser -Name $_.name `
+-Path $pfad `
+-AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
 -GivenName $_.givenname `
 -Surname $_.surname `
--Path $pfad `
 -SamAccountName $_.samAccountName `
--UserPrincipalName ($_.samAccountName + '@' + $env:userdnsdomain) `
--AccountPassword (ConvertTo-SecureString "Synergy2024!" -AsPlainText -Force) `
--EmailAddress $_."E-Mail Address" `
+-StreetAddress $_.street `
+-City $_.street `
+-State $_.state `
+-PostalCode $_.zip `
+-country $_.country `
+-EmailAddress $_."emailaddress" `
+-Description $_.description `
+-UserPrincipalName $_.upn `
 -Enabled $true `
 -ChangePasswordAtLogon $false
 } 
