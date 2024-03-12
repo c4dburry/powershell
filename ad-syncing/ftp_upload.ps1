389830631938320a9e27ae9b12d66b8db834c97c
cadburry <mathis.keyser@it-syn.de>
2024-03-12 21:17:17 +0100

    working_ftp_upload

--- ad-syncing/ftp_upload.ps1
+++ ad-syncing/ftp_upload.ps1
@@ -1,14 +1,30 @@
-#ftp shell script
+### Skript zum Upload des AD-Exports
+### 12.03.2024 - Hannover
+### Mathis Keyser - Mathis.Keyser@it-syn.de
+##Version 1.0
 
-#authentication
+#authentication + server
 $username = "user"
 $password = "password"
+$server = "server"
+$client = "testing"
 
 # files/paths
-$LocalDir = "\\Server\uploads"
-$LatestTextFile = Get-ChildItem -Path $LocalDir -Filter *.csv | Sort-Object -Property CreationTime -Descending | Select-Object -First 1
-$RemoteFile = "ftp://servername/$($LatestTextFile.Name)"
+$localdir = $PSScriptRoot
+$dir = $localdir+"\adexport\"
+# create latest export file and remote-file
+$LatestTextFile = Get-ChildItem -Path $dir -Filter *.csv | Sort-Object -Property CreationTime -Descending | Select-Object -First 1
+$RemoteFile = "ftp://$server/private/$client/$($LatestTextFile.Name)"
  
+write-host "----------RFILE------------"
+Write-Host $RemoteFile
+write-host "----------LATESTFILE------------"
+Write-Host $LatestTextFile
+write-host "--------LOCALDIR-------------"
+Write-Host $localdir
+write-host "---------DIR-----------"
+Write-Host $dir
+
 # Create FTP Request Object
 $FTPRequest = [System.Net.FtpWebRequest]::Create("$RemoteFile")
 $FTPRequest = [System.Net.FtpWebRequest]$FTPRequest
