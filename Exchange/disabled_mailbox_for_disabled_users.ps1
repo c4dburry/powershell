9acdd7cce69c6a45259b75952cedb8fa265148a6
keyserm <mathis.keyser@it-syn.de>
2024-03-13 10:31:01 +0100

    exchange-Disabled_user_script

--- Exchange/disabled_mailbox_for_disabled_users.ps1
+++ Exchange/disabled_mailbox_for_disabled_users.ps1
@@ -1,5 +1,5 @@
 $Mailboxes = Get-Mailbox -ResultSize Unlimited | ? { $_.ExchangeUserAccountControl -like "AccountDisabled" -and $_.RecipientTypeDetails -eq 'UserMailbox' } | select alias
-
+$Mailboxes | Export-Csv $PSScriptRoot\export.csv -NoTypeInformation -Encoding UTF8
 foreach ($mailbox in $Mailboxes) {
-    Disable-Mailbox -DomainController SBER01-DC01.brasst.local -mailbox $mailbox.alias -FilePath \\SBER02-EXCH01\Exports\script\$mailbox.pst
+    Disable-Mailbox -DomainController DC.domain.local -identity $mailbox.alias 
 } 
--- Exchange/export_disabled_users.ps1
+++ Exchange/export_disabled_users.ps1
@@ -1,5 +1,5 @@
 $Mailboxes = Get-Mailbox -ResultSize Unlimited | ? { $_.ExchangeUserAccountControl -like "AccountDisabled" -and $_.RecipientTypeDetails -eq 'UserMailbox' } | select alias
 
 foreach ($mailbox in $Mailboxes) {
-    New-MailboxExportRequest -DomainController SBER01-DC01.brasst.local -mailbox $mailbox.alias -FilePath \\SBER02-EXCH01\Exports\script\$mailbox.pst
+    New-MailboxExportRequest -DomainController DC.domain.local -mailbox $mailbox.alias -FilePath \\netshare\Exports\script\$mailbox.pst
 } 
